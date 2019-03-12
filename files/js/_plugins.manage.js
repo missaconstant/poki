cbc66d002e40cbe7894981034ab7589c69b6e547
Missa Constant <missabaihconstant@gmail.com>
2019-03-12 07:54:49 +0000

    transiting

--- files/js/_plugins.manage.js
+++ files/js/_plugins.manage.js
@@ -4,7 +4,7 @@ $(function () {
         e.preventDefault();
         loader.show();
 
-        postize('/plugins/toggle/' + this.parentNode.id, 'get', false, function (datas) {
+        postize(baseroute + '/plugins/toggle/' + this.parentNode.id, 'get', false, function (datas) {
             alerter.success(datas.message);
             setTimeout(function () { window.location.href = '/listener/list' }, 1500);
         },
@@ -20,7 +20,7 @@ $(function () {
         var self = this;
 
         warningAction(function () {
-            postize('/plugins/delete/' + self.parentNode.id, 'get', false, function (datas) {
+            postize(baseroute + '/plugins/delete/' + self.parentNode.id, 'get', false, function (datas) {
                 alerter.success(datas.message);
                 setTimeout(function () { window.location.href = '/listener/list' }, 1500);
             },
@@ -29,6 +29,43 @@ $(function () {
                 loader.hide();
             }, true);
         });
+    });
+
+    $('.toggle-install-form').on('click', function () { $('.install-fields').slideToggle() });
+
+    $('.installbtn').on('click', function () {
+        var files = document.getElementById('pluginfile').files;
+        
+        if (!files.length) {
+            alerter.error("You have to choose a file !");
+        }
+        else if (!/zip/.test(files[0].type.toLowerCase())) {
+            alerter.error("Zip file required !");
+        }
+        else {
+            var fd = new FormData();
+                fd.append("plugin", files[0]);
+            
+            loader.show();
+
+            $.ajax({
+                url: baseroute + '/plugins/add',
+                method: 'post',
+                contentType: false,
+                processData: false,
+                cache: false,
+                data: fd,
+                dataType: 'json',
+                success: function (datas) {
+                    console.log(datas);
+                    loader.hide();
+                },
+                error: function (err) {
+                    console.log(err);
+                    loader.hide();
+                }
+            });
+        }
     });
 
 });
